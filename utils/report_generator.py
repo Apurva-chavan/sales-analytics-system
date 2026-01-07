3822116d85af0be4b3f582306ec767ffc1113794
Apurva-chavan <apurvachavan306@email.com>
2026-01-07 14:14:01 +0530

    Add report generator description

--- utils/report_generator.py
+++ utils/report_generator.py
@@ -1,3 +1,5 @@
+# Generates text-based sales reports
+
 def generate_report(transactions):
     with open("output/sales_report.txt", "w", encoding="utf-8") as file:
         file.write("SALES ANALYTICS REPORT\n")
